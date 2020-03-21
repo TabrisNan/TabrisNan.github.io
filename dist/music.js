e523d102c19c1b89cf5f1ff1640be3f249f38dd9
TabrisChen <chenjianan1096@gmail.com>
2020-03-21 14:09:29 +0800

    Site updated: 2020-03-21 14:09:26

--- dist/music.js
+++ dist/music.js
@@ -9,27 +9,35 @@ const ap = new APlayer({
     // theme: '#FADFA3', //列表标签颜色,audio有设置theme这个就会失效
     loop: 'all',
     lrcType: 1,
-    // volume: 0.7, //默认音量
+    volume: 0.4, //默认音量
     audio: [{
-        theme: 'pink',
-        name: '圈住你',
-        artist: '一口甜',
-        url: 'https://music.163.com/song/media/outer/url?id=1372552573',
-        cover: 'http://p1.music.126.net/Y05iUqwPp1IBwCj291_Ulg==/109951164156398679.jpg?param=130y130',
+        theme: 'blue',
+        name: '明日への手紙 (写给明天的信)',
+        artist: '手嶌葵 (てしま あおい)',
+        url: 'https://music.163.com/song/media/outer/url?id=28850646',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M0000043adKo2JEAea_1.jpg?max_age=2592000',
         lrc: ""
-      }, {
-        theme: '#46718b',
-        name: '追',
-        artist: '陈壹千',
-        url: 'https://music.163.com/song/media/outer/url?id=1358848433',
-        cover: 'http://p2.music.126.net/Ax-zrmAPBrASWxT92t3fdw==/109951164000242827.jpg?param=130y130',
+      }, 
+      {
+        theme: 'blue',
+        name: '어른 (大人) (《我的大叔》韩剧插曲)',
+        artist: ' Sondia (손디아)',
+        url: 'https://sharefs.yun.kugou.com/202003211406/3ea73ed3656d0385778d1f73b28156f1/G128/M0B/03/05/IIcBAFq8oXKALa4-AD94VdJd13Q159.mp3',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000000Mix2w1utUOr_1.jpg?max_age=2592000',
       },
       {
-        name: "冬眠",
-        artist: '司南',
-        url: 'https://music.163.com/song/media/outer/url?id=1398663411',
-        cover: 'http://p2.music.126.net/4KDBaQXnQywQovmqvjx-8Q==/109951164444131697.jpg?param=130y130',
-        // theme: '#fff' //进度条颜色
+        theme: 'blue',
+        name: 'プロローグ',
+        artist: 'Uru',
+        url: 'https://music.163.com/song/media/outer/url?id=1321382214',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000001kUri631QieH_1.jpg?max_age=2592000',
+      },
+      {
+        theme: 'blue',
+        name: '혜화동 (혹은 쌍문동) (惠化洞 （或双门洞）)',
+        artist: '朴宝蓝 (박보람)',
+        url: 'http://fdfs.xmcdn.com/group63/M0B/99/33/wKgMcl0yYNOi0BWnACA_VHvbwzo444.mp3',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000003SV03p4RPnha_1.jpg?max_age=2592000',
       }
     ]
   });
